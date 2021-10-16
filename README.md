57cb9acb4838b26b4fb34c1c7bddc1b77272f13f
“tuantd16”  <tuantd16@fpt.com.vn>
2021-10-17 01:53:25 +0700

    first commit demo rails login

---
+++ README.md
@@ -0,0 +1,9 @@
+# README
+
+Demo login ruby on rails example
+DB postgresql
+
+rails s
+
+http://localhost:3000/login
+
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get     "login"    => "sessions#new"
+  post    "login"    => "sessions#create"
+  delete  "logout"   => "sessions#destroy"
+  # get 'sessions/new'
+  resources :users
+end
