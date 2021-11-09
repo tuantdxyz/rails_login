7f32c194820ac66376a2effb41f14d414116cb4a
tuantd16 <tuantd16@fpt.com.vn>
2021-11-09 13:38:18 +0700

    add seeds db

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # README
 
+# Description
 Demo login ruby on rails example
 DB postgresql
-
-rails s
-
-http://localhost:3000/login
+TODO fix bug registry
+TODO Grpc
+ex: https://viblo.asia/p/series-huong-dan-lap-trinh-ruby-on-rails-phan-1-mrDkMrrNvzL
+# How to test
+1. edit database.yml <schema,user,pass>
+2. rake db:seed
+3. rails db:migrate
+4. rails s
+5. http://localhost:3000/login
+6. TODO bug registry 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+connection = ActiveRecord::Base.connection()
+
+sql = <<-EOL
+INSERT INTO users(name, password_digest, created_at,updated_at)
+VALUES ('tuantd', '$2a$12$.akpLZiHAW8PDh2.KdlvceNvLcL8AufglDHP5iTgIj1JK/1tiaSP6', current_timestamp,current_timestamp);
+EOL
+
+sql.split(';').each do |s|
+    connection.execute(s.strip) unless s.strip.empty?
+end
